817e87805569455bd9786f8eef763f50ec34642f
Hirusha Ranaweera <leavingnadir.inc@gmail.com>
2026-05-29 18:01:50 +0530

    Updating Constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -607,6 +607,7 @@ export const certifications = [
   { img: "./assets/certifications/lfel1004-authentication-authorization-for-web-api.png", name : "Authentication and Authorization for Web APIs" },
   { img: "./assets/certifications/lfs158-introduction-to-kubernetes.png", name : "Introduction to Kubernetes" },
   { img: "./assets/certifications/lfs157-introduction-to-serverless-on-kubernetes.png", name : "Introduction to Serverless on Kubernetes" },
+  { img: "./assets/certifications/lfs147-introduction-to-ai-ml-toolkits-with-kubeflow.png", name : "Introduction to AI/ML Toolkits with Kubeflow" },
   { img: "./assets/certifications/inbound-marketing-certified.png", name : "Inbound Marketing Certified" },
   { img: "./assets/certifications/sales-management-certified.png", name : "Hubspot Sales Management Certified" },
   { img: "./assets/certifications/Navigating-On-Page-Technical-SEO-A-Semrush-Deep-Dive-certified.png", name : "Semrush Technical SEO" },
